81a5671cac9d991fa83ea0b3c73d08eeeb43d2e3
Sarah George <sngeorge27@gmail.com>
2019-07-16 14:03:18 -0400

    created magic 8 ball app

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let imagesArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
 
+    var randomBallImage: Int = 0
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        newBallImage()
         // Do any additional setup after loading the view.
     }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        newBallImage()
+    }
 
-
+    @IBAction func askButtonPressed(_ sender: Any) {
+        newBallImage()
+    }
+    
+    func newBallImage(){
+        randomBallImage = Int.random(in: 0 ... 4)
+        imageView.image = UIImage(named: imagesArray[randomBallImage])
+    }
+    
 }
 
